3bb34fc916695e6bb916f13ec55c98de925193f6
oliverhaugh <121741395+oliverhaugh@users.noreply.github.com>
2023-04-19 23:05:54 -0400

    Added strlen

--- c.py
+++ c.py
@@ -3,10 +3,17 @@
 # this is a C library for python for those C developers who are using python and miss C
 # also for developers who only know python who want to use C but don't want to learn it
 
-# printf (print())
-def printf(value):
-    print(value)
+class c:
+    # printf (print())
+    def printf(value):
+        print(value)
 
-# scanf (input())
-def scanf(input_string, variable_to_store_information):
-    value = input(input_string)
+    # scanf (input())
+    def scanf(input_string, variable_to_store_information):
+        value = input(input_string)
+
+    # strlen (len())
+    def strlen(string):
+        value = len(string)
+        return value
+    
