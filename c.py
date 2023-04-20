8d6dcc46de5825872acf427539c3f0e0b65432e0
oliverhaugh <121741395+oliverhaugh@users.noreply.github.com>
2023-04-19 22:45:37 -0400

    Add files via upload

---
+++ c.py
@@ -0,0 +1,12 @@
+# Made by OliverH
+# date started 4/19/2023
+# this is a C library for python for those C developers who are using python and miss C
+# also for developers who only know python who want to use C but don't want to learn it
+
+# printf (print())
+def printf(value):
+    print(value)
+
+# scanf (input())
+def scanf(input_string, variable_to_store_information):
+    value = input(input_string)
